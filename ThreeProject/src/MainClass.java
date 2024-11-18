2efce12be1183431b88ec543914409035d20b25b
sist <sist@DESKTOP-L0BJ8JE>
2024-11-18 16:50:26 +0900

    20241118

--- ThreeProject/src/MainClass.java
+++ ThreeProject/src/MainClass.java
@@ -1,4 +1,28 @@
 /*
+ 
+ 1. 자바 설치
+ 
+ 	path : c:\jdk-17.0.2\bin ==> java/javac
+ 	classpath : 자바에서 지원하는 라이브러리를 사용
+ 				.;c:\jdk-17.0.2\lib
+ 				.은 현재 폴더이다
+ 				
+ 2. 이클립스
+ 	1) file  => new => JavaProject
+ 	2) src안에 자바파일 생성
+ 	
+ 	변수 / 연산자 / 제어문 / 배열
+ 	public class ClassName{
+ 		프로그램의 시작
+ 		public static void main(String[] args)
+ 		}
+ 		   //시작 => 코딩
+  		{
+ 	}
+ 	
+ 3. git
+ => team ==> sharePorject
+ => team ==> commit 
  자바실행
  ======
  ** 면접  
