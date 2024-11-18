8e502397573f480180b915cc55aede052cd3855e
sist <sist@DESKTOP-L0BJ8JE>
2024-11-18 14:15:26 +0900

    first commit

---
+++ FirstProject/src/첫번째자바.java
@@ -0,0 +1,80 @@
+/*
+	1. 대소문자 구분
+	2. 들여쓰기 => 가독성 
+	3. 명령문이 종료 반드시 ;
+		; => 파이썬 / Kotlin /javascript (; 사용하지 않는다)
+	4. 자바는 {
+	5. 문법
+		데이터 저장 ====> 데어터 처리(요청) ====> 화면 출력
+			|				|				  |
+					   연산자 / 제어문			명령프롬프트 "Application
+					   							문법
+					   ============			브라우저 출력 : Web Application
+					   							주력
+					   		|				모바일 출력 : Mobile Application
+						   메소드				 Spring : Java / Kotlin
+		한개 저장(메모리):변수/상수
+		여러개 저장 : 배열
+					클래스
+		-----------------------
+			프로그램 종료시 사라진다.
+		파일 / 오라클 => 영구적인 저장
+		
+		=============> 기본문법
+		1. 기능을 모아서 관리
+		     ===
+		      => 단락 : 메소드
+		      => 클래스 :/ 객체 => 객체지향 프로그램
+		      => 프로그램 유지 (예외처리)
+		      ======================= 8장 1장~8장 공부!!
+		      => 라이브러리 : 자바에 지원
+		      => 1장 응용프로그램
+        =============> Web
+        1. HTML : 브라우저에서 실행
+        		  자바는 브라우저에서는 일반 텍스트
+           => 투박해서 => 디자인으로 CSS / 자바스크립트(예: 이미지 회전)
+        2. JSP (요청 => HTML => Java
+        		응답 => Java => HTML
+        	| MVC
+        	 =====
+        	 Front / Back
+        	   |	   | 자바/오라클 / Spring
+        	Js / TS
+        	=============================================+ Full Stack
+       	3. 전체 통합(HTML+js+jsp) : Framework : Spring
+       							  기본 틀 => 메인보드
+		4. 데이터 분석 : Python / Numpy / Pandas
+					    MatplotLib
+					    
+		5. NoSql => elasticsearch
+						| 오라클 역할
+		6. 배포 : AWS
+		=============================================================
+		 Spring-Boot / JPA / MySQL / React-Query
+		Redux / Vue3 / NextJs/ TypeScript / NodeJS(실시간 채팅)
+		
+		자바 / 오라클 / 알고리즘
+		HTML/CSS/JavaScript /Jsp
+		Spring-Boot / Python / 데이터 분석
+		elasticsearch ==> 9권 책
+		================================
+		Spring + MyBatis + JSP + JavaScript(Jquery)
+		
+		수업
+		6교시 : 수업
+		7교시 => 문제풀이
+		8교시 => 문제해결 / 하루 정리
+		=======================
+		목요일 , 금요일(1주일 정리)
+ */
+public class 첫번째자바 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        System.out.println("Hello Java");
+        System.out.println("배병우");
+        {
+        	
+        }
+	}
+}
