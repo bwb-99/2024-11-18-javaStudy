79a37ad5b6cce0fa2be2a2852642ce8e13677222
sist <sist@DESKTOP-L0BJ8JE>
2024-11-25 17:45:37 +0900

    2024-11-25

---
+++ Chapter03_제어문,_반복문/src/제어문_문제8.java
@@ -0,0 +1,25 @@
+//1~10 사이의 숫자 중 3의 배수를 제외하고 출력하라 (continue 사용)
+public class 제어문_문제8 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i=0, num=0;
+		for( i=1;i<=10;i++)
+		{
+			if(i%3!=0)
+				System.out.print(i+" ");
+				
+		}
+		/*
+		 *   for : 반복횟수가 지정된 경우 주로 for을 사용하는 경향이 있다
+		 *   => 초기식 : 시작점 => 한번만 수행
+		 *   => 조건식 : 끝점 => ture : 반복문장 실행/false:for을 종료
+		 *   => 증감식 : 횟수지정 => i++, i+=2...
+		 *   						  ==== 
+		 */
+				
+			
+		}
+
+	}
+
