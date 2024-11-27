7453604619c55ca3f17622909764704b59f599c5
sist <sist@DESKTOP-L0BJ8JE>
2024-11-28 08:56:22 +0900

    2024-11-25

--- Chapter03_제어문_반복문/src/선택문_반복문문제.java
+++ Chapter03_제어문_반복문/src/선택문_반복문문제.java
@@ -182,7 +182,7 @@ public class 선택문_반복문문제 {
         	else
         		sum+=i;
         }
-        System.out.println("sum="+sum);
+        System.out.println("sum="+sum); 
 	    //  9)	 1~10 사이의 숫자 중 3의 배수를 제외하고 출력하라
         for(int i=1;i<=10;i++)
         {
