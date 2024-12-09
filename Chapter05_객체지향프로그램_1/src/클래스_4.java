6a32b93d4f3ac7be4c8e1b201ce5350a502c950a
sist <sist@DESKTOP-L0BJ8JE>
2024-12-10 08:39:49 +0900

    2024-12-10

--- Chapter05_객체지향프로그램_1/src/클래스_4.java
+++ Chapter05_객체지향프로그램_1/src/클래스_4.java
@@ -19,23 +19,22 @@
                 </div>
             </li>
  */
-import jsoup.select.Elements;
+import org.jsoup.select.Elements;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 
 class Recipe
 {
-	int rno; //구분자
+	int rno;//구분자
 	String title;
 	String chef;
-	String poster;
 }
 class RecipeSite
 {
 	static Recipe[] recipes=new Recipe[40];
-	//사용자 정의 데이터은 일반 데이터형과 사용한는 방법 유사
-	// 시작과 동시에 recipes의 초기화
-	static // 초기화 블록
+	// 사용자 정의 데이터은 일반 데이터형과 사용하는 방법 유사 
+	// 시작과 동시에 recipes의 초기화 
+	static // 초기화 블록 
 	{
 		try
 		{
@@ -43,29 +42,32 @@ class RecipeSite
 			Document doc=Jsoup.connect("https://www.10000recipe.com/recipe/list.html").get();
 			Elements title=doc.select("ul.common_sp_list_ul div.common_sp_caption_tit");
 			Elements chef=doc.select("ul.common_sp_list_ul div.common_sp_caption_rv_name");
-			for(int i=0;i<=title.size();i++)
+			for(int i=0;i<title.size();i++)
 			{
 				recipes[j]=new Recipe();
-				recipes[j].title.get(i).text();
-				recipes[j].chef.get(i).text();
+				// 메모리 할당 
+				recipes[j].title=title.get(i).text();
+				recipes[j].chef=chef.get(i).text();
 				j++;
-//				System.out.println(title.get(i).text());
-//				System.out.println(chef.get(i).text());
+				//System.out.println(title.get(i).text());
+				//System.out.println(chef.get(i).text());
 			}
-			
 		}catch(Exception ex) {}
 	}
+	
 }
+// JSP
 public class 클래스_4 {
-	//사용자가 볼 수 있는 위치 => main => 브라우저
+
+	// 사용자가 볼 수 있는 위치 => main => 브라우저 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		RecipeSite rs=new RecipeSite();
-		System.out.println("===== 레시피 목록 =====");
-		for(Recipe r:rs.recipes)
-		{
-			System.out.println(r.title);
-		}
+        RecipeSite rs=new RecipeSite();
+        System.out.println("====== 레시피 목록 =========");
+        for(Recipe r:rs.recipes)
+        {
+        	System.out.println(r.title);
+        }
 	}
 
 }
