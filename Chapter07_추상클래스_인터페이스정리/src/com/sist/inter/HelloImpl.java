2ff6deb4312170f77ca51f54eb776a43b9401eed
sist <sist@DESKTOP-L0BJ8JE>
2024-12-17 18:02:22 +0900

    2024-12-17

---
+++ Chapter07_추상클래스_인터페이스정리/src/com/sist/inter/HelloImpl.java
@@ -0,0 +1,17 @@
+package com.sist.inter;
+//=> Service를 구현
+/*
+ * 		사용자 요청 ==> Controller ==> Service => ServiceImpl
+ * 
+ * 		MainClass ===> Hello ====> HelloImpl
+ */
+public class HelloImpl implements Hello{
+
+	@Override
+	public void seyHello(String name) {
+		// TODO Auto-generated method stub
+		System.out.println(name+"님 환영합니다!!");
+	}
+
+
+}
