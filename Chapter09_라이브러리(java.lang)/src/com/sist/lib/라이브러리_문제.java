349c03c298d0cd0e0315ff1588a4d9c0febd8367
sist <sist@DESKTOP-L0BJ8JE>
2024-12-19 17:51:35 +0900

    2024-12-19

---
+++ Chapter09_라이브러리(java.lang)/src/com/sist/lib/라이브러리_문제.java
@@ -0,0 +1,33 @@
+package com.sist.lib;
+/*
+ *   문제 1. Object 클래스 빈칸에 메소드를 넣으시요.
+ *        clone() : 해당 객체의 복제본을 생성하여 반환
+ *        equals() : 해당 객체와 절달받은 객체가 같은지 여부를 반환
+ *        finalize() : 해당객체를 더는 아무도 참조하지 않아 가비지 컬렉터가 객체의 리소스를 정리하기 위해 호출
+ *        toString() : 해당 객체의 정보를 문자열로 반환함 
+ *   
+ *   문제 2 String 클래스
+ *   
+ *         String str
+ *         charAt()
+ *         endless
+ *         length
+ *   
+ *   문제 3 Wrapper 클래스
+ *         integer
+ *         double
+ *   문제 4 
+ *       기본타입 =>   =>래퍼 클래스
+ *       래퍼 클래스 =>    =>기본타입
+ *   
+ *   문제 5  Math 클래스
+ *       
+ */        
+public class 라이브러리_문제 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
